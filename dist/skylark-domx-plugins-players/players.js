5f9516d98dfb67152550ad77639d74d2d77b2d46
hudaokeji <github@hudaokeji.com>
2021-07-03 16:59:04 +0900

    init commit

---
+++ dist/skylark-domx-plugins-players/players.js
@@ -0,0 +1,9 @@
+/**
+ * skylark-domx-plugins-players - A  ui library for rendering various media data specified by mime-type.
+ * @author Hudaokeji Co.,Ltd
+ * @version v0.9.0
+ * @link www.skylarkjs.org
+ * @license MIT
+ */
+define(["skylark-langx/skylark"],function(a){return a.attach("domx.plugins.players")});
+//# sourceMappingURL=sourcemaps/players.js.map
